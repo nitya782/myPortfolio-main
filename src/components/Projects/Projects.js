ce798e67139171a907f7c1e5052025e7da1849a4
Nitya Sharma <nsharmacs430@gmail.com>
2025-06-22 15:13:52 +0530

    final commit

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -36,10 +36,10 @@ function Projects() {
             <ProjectCard
               imgPath={bitsOfCode}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="My Portfolio"
+              description="Welcome to my portfolio — a reflection of my passion for building meaningful digital experiences. Here, you'll find a collection of my projects, creative ideas, and personal growth as a developer and problem-solver."
+              ghLink="https://github.com/nitya782/myPortfolio-main"
+              demoLink="https://my-portfolio-main-kappa-three.vercel.app/"
             />
           </Col>
 
@@ -47,44 +47,25 @@ function Projects() {
             <ProjectCard
               imgPath={editor}
               isBlog={false}
-              title="Editor.io"
-              description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              title="COVID-19 Dashboard"
+              description="The COVID-19 Dashboard is an interactive platform that presents real-time data on coronavirus cases, recoveries, and deaths. It helps users track global and country-wise trends through easy-to-read visuals and statistics."
+              ghLink="https://github.com/nitya782/COVID-19-dashboard-/blob/main/Screenshot%202025-06-22%20113509.png"
+              demoLink="https://public.tableau.com/app/profile/nitya.sharma3284/viz/COVID-19IndiaDashboard_17024747511500/Dashboard1"              
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={leaf}
-              isBlog={false}
-              title="Plant AI"
-              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={suicide}
               isBlog={false}
-              title="Ai For Social Good"
-              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              title="Calm Chatbot"
+              description="Calm Chatbot is designed to support mental health and emotional well-being by offering empathetic conversations and a safe space to express feelings. It gently guides users with comforting messages and personalized YouTube recommendations for relaxation, mindfulness, and positivity."
+              ghLink="https://github.com/nitya782/mental_health_chatbot_final"
               // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%. Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-            /> 
-          </Col>
         </Row>
       </Container>
     </Container>
